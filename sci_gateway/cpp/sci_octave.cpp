971fc665f0ce3d294b28c954a9ef66894c53707d
detailer-asus <karan.arunverma@gmail.com>
2021-04-29 17:19:39 +0530

    formated and added description

--- sci_gateway/cpp/sci_octave.cpp
+++ sci_gateway/cpp/sci_octave.cpp
@@ -17,9 +17,9 @@
 
 extern "C"
 {
-#include<Scierror.h>
-#include<sciprint.h>
-#include<api_scilab.h>
+#include <Scierror.h>
+#include <sciprint.h>
+#include <api_scilab.h>
 #include "localization.h"
 #include "fun.h"
 #include <cstdio>
@@ -28,226 +28,226 @@ extern "C"
 #include "os_string.h"
 #include <stdlib.h>
 
+	static const char fname[] = "octave_fun";
+	
+	/*! \brief Function to connect to Scilab's API.
+	* 
+	* This function will get Data from Scilab, proccess the data
+	* in Octave then return the output back to Scilab using the 
+	* API.
+	*/
+	int sci_octave_fun(scilabEnv env, int nin, scilabVar *in, int nopt, scilabOpt *opt, int nout, scilabVar *out)
 
-
-static const char fname[] = "octave_fun";
-int sci_octave_fun(scilabEnv env, int nin, scilabVar* in, int nopt, scilabOpt* opt, int nout, scilabVar* out)
-
-{
-//printf("nin: %d\n", nin);
-	if (nin < 2)
-    {
-        Scierror(999, _("%s: Wrong number of input arguments. Atleast %d expected.\n"), fname, 2);
-        return STATUS_ERROR;
-    }
-
-	FUNCCALL funcall;
-	FUNCCALL *funptr = &funcall;
-	funcall.n_in_arguments = nin;
-	funcall.n_out_user = nout;
-
-	FUNCARGS ins[funcall.n_in_arguments*nout];
-	FUNCARGS *argptr = ins;
-
-	int i,j;
-	double* d;
-	double* rd = NULL;;
-	double* cd = NULL;;
-	int size;
-	char str[20];
-	char* c;
-	double* n = NULL;
-	int row = 0;
-  	int col = 0;
-	double* in_real;
-	double* in_img;
-
-	for(i=0;i<nin;i++)
 	{
-		if(scilab_getType(env, in[i])==1)
-		{	
-			ins[i].type = TYPE_DOUBLE;
-			if(scilab_isComplex(env, in[i])==1)
+		//printf("nin: %d\n", nin);
+		if (nin < 2)
+		{
+			Scierror(999, _("%s: Wrong number of input arguments. Atleast %d expected.\n"), fname, 2);
+			return STATUS_ERROR;
+		}
+
+		FUNCCALL funcall;
+		FUNCCALL *funptr = &funcall;
+		funcall.n_in_arguments = nin;
+		funcall.n_out_user = nout;
+
+		FUNCARGS ins[funcall.n_in_arguments * nout];
+		FUNCARGS *argptr = ins;
+
+		int i, j;
+		double *d;
+		double *rd = NULL;
+		double *cd = NULL;
+		int size;
+		char str[20];
+		char *c;
+		double *n = NULL;
+		int row = 0;
+		int col = 0;
+		double *in_real;
+		double *in_img;
+
+		for (i = 0; i < nin; i++)
+		{
+			if (scilab_getType(env, in[i]) == 1)
 			{
-				//printf("input %d is complex \n", i);
-				ins[i].is_in_cmplx=1;
-				size = scilab_getDim2d(env, in[i], &row, &col);
-				ins[i].n_in_rows = row;
-				ins[i].n_in_cols = col;
-				scilab_getDoubleComplexArray(env, in[i],&in_real, &in_img);
-
-				ins[i].in_data_real = malloc(sizeof(double)*size);
-				ins[i].in_data_img = malloc(sizeof(double)*size);
-				rd = (double *)ins[i].in_data_real;
-				cd = (double *)ins[i].in_data_img;
-
-	////This code snippet is to flatten matrix row wise and then store it
-				int p,q,k = 0;
-				for(p=0;p<row;p++)
+				ins[i].type = TYPE_DOUBLE;
+				if (scilab_isComplex(env, in[i]) == 1)
 				{
-					for(q=0;q<col;q++)
+					//printf("input %d is complex \n", i);
+					ins[i].is_in_cmplx = 1;
+					size = scilab_getDim2d(env, in[i], &row, &col);
+					ins[i].n_in_rows = row;
+					ins[i].n_in_cols = col;
+					scilab_getDoubleComplexArray(env, in[i], &in_real, &in_img);
+
+					ins[i].in_data_real = malloc(sizeof(double) * size);
+					ins[i].in_data_img = malloc(sizeof(double) * size);
+					rd = (double *)ins[i].in_data_real;
+					cd = (double *)ins[i].in_data_img;
+
+					////This code snippet is to flatten matrix row wise and then store it
+					int p, q, k = 0;
+					for (p = 0; p < row; p++)
 					{
-						rd[k] = in_real[p + q*row];
-						cd[k] = in_img[p + q*row];
-						k++;
-						//printf("%d\n",in_real[k]);
-						//printf("%d\n",in_img[k]);
+						for (q = 0; q < col; q++)
+						{
+							rd[k] = in_real[p + q * row];
+							cd[k] = in_img[p + q * row];
+							k++;
+							//printf("%d\n",in_real[k]);
+							//printf("%d\n",in_img[k]);
+						}
 					}
+				}
+				else
+				{
+					//printf("input %d is NOT complex \n", i);
+					ins[i].is_in_cmplx = 0;
+					size = scilab_getDim2d(env, in[i], &row, &col);
+					ins[i].n_in_rows = row;
+					ins[i].n_in_cols = col;
+					scilab_getDoubleArray(env, in[i], &n);
+
+					ins[i].in_data_real = malloc(sizeof(double) * size);
+					d = (double *)ins[i].in_data_real;
+
+					////This code snippet is to flatten matrix row wise and then store it
+					int p, q, k = 0;
+					for (p = 0; p < row; p++)
+					{
+						for (q = 0; q < col; q++)
+						{
+							d[k] = n[p + q * row];
+							k++;
+							//printf("%f\n",d[j]);
+						}
+					}
+				}
+				/////////////////////////////////////////
+			}
+			else if (scilab_getType(env, in[i]) == 10)
+			{
+				ins[i].is_in_cmplx = 0;
+				wchar_t *in1 = 0;
+
+				scilab_getString(env, in[i], &in1);
+				//printf("%S\n", in1);
+
+				wcstombs(str, in1, sizeof(str));
+				//printf("%s\n", str);
+				if (str)
+				{
+					//printf("lenght of string input: %d\n", strlen(str));
+					ins[i].type = TYPE_STRING;
+					ins[i].n_in_rows = 1;
+					ins[i].n_in_cols = strlen(str);
+					size = (ins[i].n_in_rows) * (ins[i].n_in_cols);
+					ins[i].in_data_real = malloc(sizeof(char) * size + 1);
+					c = (char *)ins[i].in_data_real;
+					int ci;
+
+					strcpy(c, str);
+					ins[i].n_in_cols = strlen(c);
+					//printf("in scilab strin is: %s\n", c);
 				}
 			}
 			else
 			{
-				//printf("input %d is NOT complex \n", i);
-				ins[i].is_in_cmplx=0;
-				size = scilab_getDim2d(env, in[i], &row, &col);
-				ins[i].n_in_rows = row;
-				ins[i].n_in_cols = col;
-				scilab_getDoubleArray(env, in[i], &n);
+				Scierror(999, _("%s: Wrong type of input argument %d.\n"), fname, i);
+				return STATUS_ERROR;
+			}
+		}
 
-				ins[i].in_data_real = malloc(sizeof(double)*size);
-				d = (double *)ins[i].in_data_real;
+		// Capturing Errors and warnings
+		std::stringstream buffer_err;
 
-	////This code snippet is to flatten matrix row wise and then store it
-				int p,q,k = 0;
-				for(p=0;p<row;p++)
+		// set our error buffer
+		std::cerr.rdbuf(buffer_err.rdbuf());
+
+		int status_fun = fun(argptr, funptr);
+
+		// grab error buffer contents
+		std::string err = buffer_err.str();
+
+		if (!err.empty() && status_fun == 0)
+			sciprint("Warning from Octave\n%s", err.c_str());
+		buffer_err.str("");
+		//printf("in scilab status_fun is: %d\n", status_fun);
+		//printf("in scilab funcall.n_out_arguments is: %d\n", funcall.n_out_arguments);
+		//printf("in scilab funcall.n_out_user is: %d\n", funcall.n_out_user);
+		//printf("in scilab ins[0].n_out_rows is: %d\n", ins[0].n_out_rows);
+		//printf("in scilab ins[0].n_out_cols is: %d\n", ins[0].n_out_cols);
+
+		//printf("in scilab ouput args are: %d\n", funcall.n_out_arguments);
+		if (status_fun == 1)
+		{
+			Scierror(999, "Error from Octave\n%s", err.c_str());
+			return 1;
+		}
+		else if (funcall.n_out_user <= funcall.n_out_arguments)
+		{
+			for (i = 0; i < nout; i++)
+			{
+
+				if (ins[i].is_out_cmplx == 1)
 				{
-					for(q=0;q<col;q++)
+					//printf("output %d is complex\n", i);
+					out[i] = scilab_createDoubleMatrix2d(env, ins[i].n_out_rows, ins[i].n_out_cols, 1);
+					double *out_real = NULL;
+					double *out_img = NULL;
+					scilab_getDoubleComplexArray(env, out[i], &out_real, &out_img);
+					int len = ins[i].n_out_rows * ins[i].n_out_cols;
+					double *ord = (double *)ins[i].out_data_real;
+					double *ocd = (double *)ins[i].out_data_img;
+					//printf("output length is: %d\n", len);
+					for (j = 0; j < len; j++)
 					{
-						d[k] = n[p + q*row];
-						k++;
-						//printf("%f\n",d[j]);
+						out_real[j] = ord[j];
+					}
+
+					for (j = 0; j < len; j++)
+					{
+						out_img[j] = ocd[j];
+					}
+				}
+				else
+				{
+					//printf("output %d is NOT complex\n", i);
+					out[i] = scilab_createDoubleMatrix2d(env, ins[i].n_out_rows, ins[i].n_out_cols, 0);
+					double *out1 = NULL;
+					scilab_getDoubleArray(env, out[i], &out1);
+					int len = ins[i].n_out_rows * ins[i].n_out_cols;
+					double *dd = (double *)ins[i].out_data_real;
+					//printf("output length is: %d\n", len);
+					for (j = 0; j < len; j++)
+					{
+						out1[j] = dd[j]; //.float_value();
 					}
 				}
 			}
-/////////////////////////////////////////
-		}
-		else if(scilab_getType(env, in[i])==10)
-		{
-			ins[i].is_in_cmplx=0;
-			wchar_t* in1 = 0;
-
-			scilab_getString(env, in[i], &in1);
-			//printf("%S\n", in1);
-
-			wcstombs(str, in1, sizeof(str));
-			//printf("%s\n", str);
-			if(str)
-			{
-				//printf("lenght of string input: %d\n", strlen(str));
-				ins[i].type = TYPE_STRING;
-				ins[i].n_in_rows = 1;
-				ins[i].n_in_cols = strlen(str);
-				size = (ins[i].n_in_rows)*(ins[i].n_in_cols);
-				ins[i].in_data_real = malloc(sizeof(char)*size+1);
-				c = (char *)ins[i].in_data_real;
-				int ci;
-
-				strcpy(c,str);	
-				ins[i].n_in_cols = strlen(c);
-				//printf("in scilab strin is: %s\n", c);
-
-			}
 		}
 		else
 		{
-        	Scierror(999, _("%s: Wrong type of input argument %d.\n"), fname, i);
-        	return STATUS_ERROR;
-    	}
-	}
-                         
-    // Capturing Errors and warnings
-    std::stringstream buffer_err;
-
-    // set our error buffer
-	std::cerr.rdbuf(buffer_err.rdbuf());
-
-	int status_fun = fun(argptr, funptr);
-
-    // grab error buffer contents
-    std::string err = buffer_err.str(); 
-
-	if(!err.empty() && status_fun==0)
-    sciprint("Warning from Octave\n%s", err.c_str());
-    buffer_err.str("");
-		//printf("in scilab status_fun is: %d\n", status_fun);
-			//printf("in scilab funcall.n_out_arguments is: %d\n", funcall.n_out_arguments);
-			//printf("in scilab funcall.n_out_user is: %d\n", funcall.n_out_user);
-//printf("in scilab ins[0].n_out_rows is: %d\n", ins[0].n_out_rows);
-//printf("in scilab ins[0].n_out_cols is: %d\n", ins[0].n_out_cols);
-
-
-//printf("in scilab ouput args are: %d\n", funcall.n_out_arguments);
-	if(status_fun==1)
-	{
-		Scierror(999,"Error from Octave\n%s", err.c_str());
-		return 1;
-	}
-	else if(funcall.n_out_user <= funcall.n_out_arguments)
-	{	
-		for(i=0;i<nout;i++)
-		{	
-			
-		if(ins[i].is_out_cmplx==1)
-		{
-			//printf("output %d is complex\n", i);
-			out[i] = scilab_createDoubleMatrix2d(env, ins[i].n_out_rows, ins[i].n_out_cols, 1);
-			double* out_real = NULL;
-			double* out_img = NULL;
-			scilab_getDoubleComplexArray(env, out[i],&out_real, &out_img);
-			int len = ins[i].n_out_rows*ins[i].n_out_cols;
-			double* ord = (double *)ins[i].out_data_real;
-			double* ocd = (double *)ins[i].out_data_img;
-			//printf("output length is: %d\n", len);
-			for(j=0; j<len; j++)
-				{
-					out_real[j] = ord[j];
-				}
-
-			for(j=0; j<len; j++)
-				{
-					out_img[j] = ocd[j];
-				}
+			Scierror(77, _("%s: Wrong number of output arguments: This function can return a maximum of %d output(s).\n"), fname, funcall.n_out_arguments);
+			return 1;
 		}
-		else
+
+		for (i = 0; i < nout; i++)
 		{
-			//printf("output %d is NOT complex\n", i);
-			out[i] = scilab_createDoubleMatrix2d(env, ins[i].n_out_rows, ins[i].n_out_cols, 0);
-			double* out1 = NULL;
-		 	scilab_getDoubleArray(env, out[i], &out1);
-			int len = ins[i].n_out_rows*ins[i].n_out_cols;
-			double* dd = (double *)ins[i].out_data_real;
-			//printf("output length is: %d\n", len);
-			for(j=0; j<len; j++)
-				{
-					out1[j] = dd[j];//.float_value();
-				}
-			}
+			free(ins[i].out_data_real);
+
+			if (ins[i].is_out_cmplx == 1)
+				free(ins[i].out_data_img);
 		}
+
+		for (i = 0; i < nin; i++)
+		{
+			free(ins[i].in_data_real);
+
+			if (ins[i].is_in_cmplx == 1)
+				free(ins[i].in_data_img);
+		}
+		return 0;
 	}
-	else
-	{
-		Scierror(77, _("%s: Wrong number of output arguments: This function can return a maximum of %d output(s).\n"), fname, funcall.n_out_arguments);
-		return 1;
-	}
-
-
-
-
-	for(i=0;i<nout;i++)
-	{
-		free(ins[i].out_data_real);
-
-		if(ins[i].is_out_cmplx==1)
-			free(ins[i].out_data_img);
-	}
-
-	for(i=0;i<nin;i++)
-	{
-		free(ins[i].in_data_real);
-
-		if(ins[i].is_in_cmplx==1)
-			free(ins[i].in_data_img);
-	}
-  return 0;
-}
 }
--- src/fun.cpp
+++ src/fun.cpp
@@ -21,112 +21,116 @@
 
 extern "C"
 {
+	/*!
+	* \brief Function to Interact with Octave's API.
+	*
+	* This Function will be communicating with Octave to access it's function.
+	*/
 	int fun(FUNCARGS *inp, FUNCCALL *funcall)
 	{
 
 		static octave::interpreter interpreter;
 		bool status = interpreter.initialized();
-		
-		if(status==false)
+
+		if (status == false)
 		{
-		interpreter.initialize ();
-		int status_exec = interpreter.execute ();
+			interpreter.initialize();
+			int status_exec = interpreter.execute();
 
 			if (status_exec != 0)
 			{
 				std::cerr << "creating embedded Octave interpreter failed!"
-						<< std::endl;
+						  << std::endl;
 			}
 		}
-		
+
 		try
-		{	
+		{
 			octave_value_list in;
 			unsigned int k;
 			int l;
 			int str_count = 0;
 			char str_fun[20];
 			char str_pkg[20];
-			int pkg=0;
+			int pkg = 0;
 			int nouts;
-			for(l=0;l<funcall->n_in_arguments;l++)
+			for (l = 0; l < funcall->n_in_arguments; l++)
 			{
-				if(inp[l].type==TYPE_DOUBLE)
+				if (inp[l].type == TYPE_DOUBLE)
 				{
-					if(inp[l].is_in_cmplx==1)
+					if (inp[l].is_in_cmplx == 1)
 					{
-						ComplexMatrix matr = ComplexMatrix (inp[l].n_in_rows,inp[l].n_in_cols); 
-						double* id_real = (double *)inp[l].in_data_real;
-						double* id_img = (double *)inp[l].in_data_img;
-						k=0;
-						for (int r=0;r<inp[l].n_in_rows;r++) 
-							{ 
-							for(int c=0;c<inp[l].n_in_cols;c++) 
-								{ 
-								Complex cc(id_real[k],id_img[k]); 
-								matr(r,c) = cc;
-								k++;
-								} 
-							} 
-						in(l-str_count) = octave_value(matr);
-					}
-					else
-					{
-						Matrix inMatrix_x(inp[l].n_in_rows,inp[l].n_in_cols);
-						double* id = (double *)inp[l].in_data_real;
-						k=0;
-						for( unsigned int i = 0; i < inp[l].n_in_rows; i++ )
+						ComplexMatrix matr = ComplexMatrix(inp[l].n_in_rows, inp[l].n_in_cols);
+						double *id_real = (double *)inp[l].in_data_real;
+						double *id_img = (double *)inp[l].in_data_img;
+						k = 0;
+						for (int r = 0; r < inp[l].n_in_rows; r++)
 						{
-							for( unsigned int j = 0; j < inp[l].n_in_cols; j++ )
+							for (int c = 0; c < inp[l].n_in_cols; c++)
 							{
-									inMatrix_x(i, j) = id[k];
-									k++;
+								Complex cc(id_real[k], id_img[k]);
+								matr(r, c) = cc;
+								k++;
 							}
 						}
-						in(l-str_count) = inMatrix_x;
+						in(l - str_count) = octave_value(matr);
+					}
+					else
+					{
+						Matrix inMatrix_x(inp[l].n_in_rows, inp[l].n_in_cols);
+						double *id = (double *)inp[l].in_data_real;
+						k = 0;
+						for (unsigned int i = 0; i < inp[l].n_in_rows; i++)
+						{
+							for (unsigned int j = 0; j < inp[l].n_in_cols; j++)
+							{
+								inMatrix_x(i, j) = id[k];
+								k++;
+							}
+						}
+						in(l - str_count) = inMatrix_x;
 					}
 				}
-				else if(inp[l].type==TYPE_STRING)
+				else if (inp[l].type == TYPE_STRING)
 				{
 					//std::cout << "In fun string. l is : " << l << '\n';
-					
-					char* c = (char *)inp[l].in_data_real;
+
+					char *c = (char *)inp[l].in_data_real;
 					//std::cout << "String is: " << c << '\n';
-					if(l==0)
-						strcpy(str_fun,c);
-					else if(l==1)
+					if (l == 0)
+						strcpy(str_fun, c);
+					else if (l == 1)
 					{
-						strcpy(str_pkg,c);
-						pkg=1;
-					}					
+						strcpy(str_pkg, c);
+						pkg = 1;
+					}
 					else
-						in(l-str_count) = c;
+						in(l - str_count) = c;
 
 					str_count++;
 					//std::cout << "String is: " << c << '\n';
 				}
 			}
 
-			if(pkg==1)
+			if (pkg == 1)
 			{
 				//std::cout << "loading package " << str_pkg << '\n';
-				octave::feval ("pkg", ovl ("load", str_pkg), 0);
+				octave::feval("pkg", ovl("load", str_pkg), 0);
 			}
 
-			octave_value_list out = octave::feval (str_fun, in, funcall->n_out_user);
-
+			octave_value_list out = octave::feval(str_fun, in, funcall->n_out_user);
 
 			int row;
 			int col;
 			nouts = out.length();
 			funcall->n_out_arguments = nouts;
-//std::cout << "funcall->n_out_arguments is: " << funcall->n_out_arguments << '\n';
+			//std::cout << "funcall->n_out_arguments is: " << funcall->n_out_arguments << '\n';
 
-			for( unsigned int ii = 0; ii < nouts; ii++ )
+			for (unsigned int ii = 0; ii < nouts; ii++)
 			{
-				if(out(ii).iscomplex()==1)
+				if (out(ii).iscomplex() == 1)
 				{
-					inp[ii].is_out_cmplx=1;
+					inp[ii].is_out_cmplx = 1;
 					//std::cout << "out "<< ii<< " is complex" << '\n';
 					ComplexMatrix cmOut(out(ii).complex_matrix_value());
 					//std::cout << "cmOut "<< cmOut << '\n';
@@ -136,17 +140,17 @@ extern "C"
 					col = cmOut.columns();
 					inp[ii].n_out_rows = row;
 					inp[ii].n_out_cols = col;
-					k=0;
-					inp[ii].out_data_real = malloc(sizeof(double)*(row*col));
-					inp[ii].out_data_img = malloc(sizeof(double)*(row*col));
-					double* rd = (double *)inp[ii].out_data_real;
-					double* cd = (double *)inp[ii].out_data_img;
-					for(unsigned int i=0;i<row;i++)
+					k = 0;
+					inp[ii].out_data_real = malloc(sizeof(double) * (row * col));
+					inp[ii].out_data_img = malloc(sizeof(double) * (row * col));
+					double *rd = (double *)inp[ii].out_data_real;
+					double *cd = (double *)inp[ii].out_data_img;
+					for (unsigned int i = 0; i < row; i++)
 					{
-						for(unsigned int j=0;j<col;j++)
+						for (unsigned int j = 0; j < col; j++)
 						{
-							rd[k]=real(cmOut(k));
-							cd[k]=imag(cmOut(k));
+							rd[k] = real(cmOut(k));
+							cd[k] = imag(cmOut(k));
 							//std::cout << "out img "<< k << " is :" << (double)imag(cmOut(k)) << '\n';
 							k++;
 						}
@@ -155,33 +159,33 @@ extern "C"
 				else
 				{
 					//std::cout << "out "<< ii<< " is NOT complex" << '\n';
-					inp[ii].is_out_cmplx=0;
+					inp[ii].is_out_cmplx = 0;
 					Matrix mOut(out(ii).matrix_value());
 					row = mOut.rows();
 					col = mOut.columns();
 					inp[ii].n_out_rows = row;
 					inp[ii].n_out_cols = col;
-					k=0;
-					inp[ii].out_data_real = malloc(sizeof(double)*(row*col));
-					double* dd = (double *)inp[ii].out_data_real;
-					for(unsigned int i=0;i<row;i++)
+					k = 0;
+					inp[ii].out_data_real = malloc(sizeof(double) * (row * col));
+					double *dd = (double *)inp[ii].out_data_real;
+					for (unsigned int i = 0; i < row; i++)
 					{
-						for(unsigned int j=0;j<col;j++)
+						for (unsigned int j = 0; j < col; j++)
 						{
-							dd[k]=mOut(k);
+							dd[k] = mOut(k);
 							k++;
 						}
 					}
 				}
 			}
 		}
-		catch (const octave::exit_exception& ex)
+		catch (const octave::exit_exception &ex)
 		{
 			std::cerr << "Octave interpreter exited with status = "
-					<< ex.exit_status () << std::endl;
+					  << ex.exit_status() << std::endl;
 			return 1;
 		}
-		catch (const octave::execution_exception&)
+		catch (const octave::execution_exception &)
 		{
 			//std::cerr << "error encountered in Octave evaluator!" << std::endl;
 			return 1;
